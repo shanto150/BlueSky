4a26fc40314f2c4a41eba09bb8e55b149933d3f0
atiq012 <shakilratiq@gmail.com>
2024-10-21 13:30:59 +0600

    role create

---
+++ app/Models/Role/RolePermission.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Role;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class RolePermission extends Model
+{
+    use HasFactory;
+}
