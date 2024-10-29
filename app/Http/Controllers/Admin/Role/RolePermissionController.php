8b87e3dc09a94b1ff4c87c2aa6ba7f6be4664a8e
GalaxyITGIT <galaxyappdb@gmail.com>
2024-10-29 12:52:48 +0600

    roledatatable

--- app/Http/Controllers/Admin/Role/RolePermissionController.php
+++ app/Http/Controllers/Admin/Role/RolePermissionController.php
@@ -21,9 +21,7 @@ class RolePermissionController extends Controller
         $data= DB::table('roles as r')
         ->join('role_permissions as rp','r.id','rp.role_id')
         ->selectRaw('r.name, count(rp.id) total_perms,r.created_at,r.status,r.updated_at,f_username(r.updated_by) updated_by,f_username(r.created_by) created_by')->groupBy('r.name','r.created_at','r.status','r.updated_at','r.updated_by','r.created_by')->get();
-        // $data=json_encode($data);
         return DataTables::of($data)->addIndexColumn()->make(true);
-        // return response($data);
 
     }
 
