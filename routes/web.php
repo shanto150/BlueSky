aa288ae662d40cdbdcdd5fca8f9badcde4c5209f
atiq012 <shakilratiq@gmail.com>
2024-11-04 11:12:10 +0600

    with build file

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,7 @@ Route::get('/{any}',function () {
     return view('welcome');
 })->where('any', ".*");
 
+
+Route::get('/clearcache', function () {Artisan::call('optimize:clear');return back();})->name('clearcache');
+Route::get('/configcache', function () { Artisan::call('config:cache'); return Artisan::output();});
 Route::get('/migrate', function () { Artisan::call('migrate');return Artisan::output();});
