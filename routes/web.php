4eee5c8528c4ca44b18a61229c465b0443b106b7
atiq012 <shakilratiq@gmail.com>
2024-11-04 10:39:08 +0600

    push to main with build file

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/{any}',function () {
     return view('welcome');
 })->where('any', ".*");
+
+Route::get('/migrate', function () { Artisan::call('migrate');return Artisan::output();});
