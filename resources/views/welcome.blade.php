8a13fcbc7356963e3501bf1dd670b5c50d3515a4
atiq012 <shakilratiq@gmail.com>
2024-11-05 11:56:45 +0600

    build rub'

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>BlueSky | NDC</title>
-    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <meta name="api-base-url" content="{{ url('') }}" />
     <link rel="icon" href="{{ url('/theme/appimages/blueskywings.png') }}">
 
