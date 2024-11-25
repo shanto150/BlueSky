34ec540f7fe417347414bda2d9bd93e8da26dc7f
GalaxyITGIT <galaxyappdb@gmail.com>
2024-11-25 17:47:09 +0600

    afterliveupdate

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
     build: {
-        chunkSizeWarningLimit: 1600
+        chunkSizeWarningLimit: 1600,
     },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
         vue(),
