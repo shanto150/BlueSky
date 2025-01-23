5016dc29d9dc9dd033ae2947389aa5254545601a
atiq012 <shakilratiq@gmail.com>
2025-01-23 10:57:52 +0600

    migration file

---
+++ app/Http/Controllers/Admin/AirlineLogo/AirlineLogoController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Admin\AirlineLogo;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\BaseController;
+
+class AirlineLogoController extends BaseController
+{
+    //
+}
