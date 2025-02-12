b2ef4c3c6596a445b99810a1de2bc31e40b25fc2
Md Shakaouth Hossain <shanto930@gmail.com>
2025-02-12 19:06:42 +0600

    listdata

--- app/Http/Controllers/Admin/API/APIController.php
+++ app/Http/Controllers/Admin/API/APIController.php
@@ -17,17 +17,26 @@ class APIController extends BaseController {
             'to' => 'required',
         ] );
 
-        $url = 'https://apac.universal-api.pp.travelport.com/B2BGateway/connect/uAPI/AirService';
         $requestXML = new RequestXML();
         $xmlpayload = $requestXML->generateLowFareSearchXML( $request );
 
         // dd( $xmlpayload );
+        $array = $this->performCurlRequest( $xmlpayload );
+        // dd($array);
+        $sRjson= new Singlewayjson();
+        $json = $sRjson->checkJson( $array, $request );
+
+        return  $json;
+
+    }
+
+    public function performCurlRequest($xmlpayload) {
 
         $username = env( 'API_USERNAME' );
         $password = env( 'API_PASSWORD' );
 
+        $url = 'https://apac.universal-api.pp.travelport.com/B2BGateway/connect/uAPI/AirService';
         $ch = curl_init();
-
         // Set cURL options
         curl_setopt( $ch, CURLOPT_URL, $url );
         curl_setopt( $ch, CURLOPT_POST, true );
@@ -55,136 +64,14 @@ class APIController extends BaseController {
         //good format****
         $response = preg_replace( '/(<\/?)(\w+):([^>]*>)/', "$1$2$3", $response );
         $xml = new SimpleXMLElement( $response );
-        $body = $xml->xpath( '//SOAPBody' )[0];
+        $body = $xml->xpath( '//SOAPBody' )[ 0 ];
+        //return $boday as simaple json not array
+        // $jsonData = json_encode( $body );
+        // dd( $jsonData );
         $array = json_decode( json_encode( ( array )$body ), TRUE );
         //End good ******
 
-        $jsonData = $array;
-
-        // dd( $jsonData );
-
-        if ( !$jsonData ) {
-            die( json_encode( [ 'error' => 'Unable to load JSON file.' ] ) );
-        }
-
-        // Extract necessary data
-        $segments = isset( $jsonData[ 'airLowFareSearchRsp' ][ 'airAirSegmentList' ][ 'airAirSegment' ] ) ? $jsonData[ 'airLowFareSearchRsp' ][ 'airAirSegmentList' ][ 'airAirSegment' ] : [];
-        $pricingSolutions = isset( $jsonData[ 'airLowFareSearchRsp' ][ 'airPriceSolutionList' ][ 'airPriceSolution' ] ) ? $jsonData[ 'airLowFareSearchRsp' ][ 'airPriceSolutionList' ][ 'airPriceSolution' ] : [];
-
-        // Get filters from input
-        $filterOrigin = isset( $request->from ) ? $request->from : 'DAC';
-        $filterDestination = isset( $request->to ) ? $request->to : 'DFW';
-        $filterAirline = isset( $request->airline ) ? $request->airline : '';
-
-        // dd( $filterOrigin, $filterDestination, $filterAirline );
-
-        // Find all connections
-
-        function findConnections( $segments, $origin, $destination, $filterAirline = '' ) {
-            $connections = [];
-            foreach ( $segments as $segment ) {
-                $segOrigin = $segment[ '@attributes' ][ 'Origin' ];
-                $segDestination = $segment[ '@attributes' ][ 'Destination' ];
-                $segCarrier = $segment[ '@attributes' ][ 'Carrier' ];
-
-                if ( $segOrigin == $origin && ( $filterAirline == '' || $segCarrier == $filterAirline ) ) {
-                    $remainingConnections = findConnections( $segments, $segDestination, $destination, $filterAirline );
-                    if ( $segDestination == $destination || $remainingConnections ) {
-                        $connections[] = [
-                            'segment' => $segment,
-                            'connections' => $remainingConnections
-                        ];
-                    }
-                }
-            }
-            return $connections;
-        }
-
-        // Flatten connections into combinations
-
-        function flattenConnections( $connections, $currentPath = [] ) {
-            $combinations = [];
-            foreach ( $connections as $connection ) {
-                $path = array_merge( $currentPath, [ $connection[ 'segment' ] ] );
-                if ( empty( $connection[ 'connections' ] ) ) {
-                    $combinations[] = $path;
-                } else {
-                    $combinations = array_merge( $combinations, flattenConnections( $connection[ 'connections' ], $path ) );
-                }
-            }
-            return $combinations;
-        }
-
-        // Calculate flight duration
-
-        function calculateFlightDuration( $departure, $arrival ) {
-            $departureTime = new DateTime( $departure );
-            $arrivalTime = new DateTime( $arrival );
-            $interval = $departureTime->diff( $arrivalTime );
-            return $interval->h * 60 + $interval->i;
-            // Total minutes
-        }
-
-        // Format duration in hours and minutes
-
-        function formatDuration( $totalMinutes ) {
-            $hours = floor( $totalMinutes / 60 );
-            $minutes = $totalMinutes % 60;
-            return sprintf( '%dh %dm', $hours, $minutes );
-        }
-
-        // Find ADT pricing information
-
-        function findPricing( $pricingSolutions ) {
-            foreach ( $pricingSolutions as $solution ) {
-                foreach ( isset( $solution[ 'airPricingInfo' ] ) ? $solution[ 'airPricingInfo' ] : [] as $info ) {
-                    if ( isset( $info[ 'passengerType' ] ) && $info[ 'passengerType' ] === 'ADT' ) {
-                        return isset( $info[ '@attributes' ][ 'TotalPrice' ] ) ? $info[ '@attributes' ][ 'TotalPrice' ] : 'N/A';
-                    }
-                }
-            }
-            return 'N/A';
-        }
-
-        // Generate connections and combinations
-        $connections = findConnections( $segments, $filterOrigin, $filterDestination, $filterAirline );
-        $combinations = flattenConnections( $connections );
-
-        // Prepare JSON output
-        $response = [];
-        foreach ( $combinations as $combo ) {
-            $firstSegment = $combo[ 0 ][ '@attributes' ];
-            $lastSegment = $combo[ count( $combo ) - 1 ][ '@attributes' ];
-            $totalDuration = 0;
-            $connectionCount = count( $combo ) - 1;
-
-            // dd( $combo );
-
-            foreach ( $combo as $segment ) {
-                $segmentAttributes = $segment[ '@attributes' ];
-                $totalDuration += calculateFlightDuration( $segmentAttributes[ 'DepartureTime' ], $segmentAttributes[ 'ArrivalTime' ] );
-            }
-
-            $response[] = [
-                'origin' => $firstSegment[ 'Origin' ],
-                'destination' => $lastSegment[ 'Destination' ],
-                'departure_date' => substr( $firstSegment[ 'DepartureTime' ], 0, 10 ),
-                'departure_time' => substr( $firstSegment[ 'DepartureTime' ], 11, 5 ),
-                'arrival_date' => substr( $lastSegment[ 'ArrivalTime' ], 0, 10 ),
-                'arrival_time' => substr( $lastSegment[ 'ArrivalTime' ], 11, 5 ),
-                'carrier_code' => $firstSegment[ 'Carrier' ],
-                'flight_numbers' => $firstSegment[ 'FlightNumber' ] . '-' . $lastSegment[ 'FlightNumber' ],
-                'total_flight_duration' => formatDuration( $totalDuration ),
-                'connections' => $connectionCount,
-                'total_price_ADT' => findPricing( $pricingSolutions ),
-            ];
-        }
-
-        header( 'Content-Type: application/json' );
-        // echo json_encode( [ 'flights' => $response ], JSON_PRETTY_PRINT );
-
-        return response()->json( [ 'flights' => $response ], 200, [], JSON_PRETTY_PRINT );
-
+        return  $array;
     }
 
 }
